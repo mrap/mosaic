062c66d63df45b272ffedba8329a9c9870608d05
Michael Rapadas <michael.rapadas@otoy.com>
2015-05-13 00:51:31 -0700

    Create Lab from XYZ

---
+++ color/cie/lab.go
@@ -0,0 +1,43 @@
+package cie
+
+import (
+	"math"
+)
+
+// Observer= 2°, Illuminant= D65
+var xyzRef = XYZ{
+	X: 95.047,
+	Y: 100.000,
+	Z: 108.883,
+}
+
+type Lab struct {
+	L int8
+	A int8
+	B int8
+}
+
+func LabFromXYZ(c XYZ) Lab {
+	x := convertLabFromXYZ(c.X / xyzRef.X)
+	y := convertLabFromXYZ(c.Y / xyzRef.Y)
+	z := convertLabFromXYZ(c.Z / xyzRef.Z)
+
+	return Lab{
+		L: int8(116*y - 16),
+		A: int8(500 * (x - y)),
+		B: int8(200 * (y - z)),
+	}
+}
+
+const (
+	epsilon = 0.008856
+	kappa   = 903.3
+)
+
+func convertLabFromXYZ(v float32) float32 {
+	if v > epsilon {
+		return float32(math.Pow(float64(v), 1/3))
+	} else {
+		return kappa*v + 16/116
+	}
+}
