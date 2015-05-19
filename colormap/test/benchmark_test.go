599b1bbf97aee1e1eca4f8fe764ac00bd9db117e
Michael Rapadas <michael.rapadas@otoy.com>
2015-05-19 06:23:48 -0700

    Adds benchmark for creating a colormapped image

--- colormap/test/benchmark_test.go
+++ colormap/test/benchmark_test.go
@@ -6,6 +6,12 @@ import (
 
 const waterdropImgPath = "images/waterdrop_500x500.jpg"
 
+func BenchmarkImageFileToColorMap(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		imageColorMap(waterdropImgPath)
+	}
+}
+
 func BenchmarkImageFileToTopPalette(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		cmap, _ := imageColorMap(waterdropImgPath)
